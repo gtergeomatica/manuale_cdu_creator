2b48b1bd033eeaacc508944413ed6a85f73883db
RobiFag <robifagandini@hotmail.it>
2020-04-20 18:55:13 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -85,8 +85,8 @@ pygments_style = None
 #html_theme = 'alabaster'
 
 #standard readthedocs
-#html_theme = "sphinx_rtd_theme"
-html_theme = "rtd_gter"
+html_theme = "sphinx_rtd_theme"
+#html_theme = "rtd_gter"
 
 html_title = ""
 
